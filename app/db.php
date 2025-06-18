a950264e239e6f3dcd098c8e7a44e2ae9d67c96a
Faiza467 <faizamarrium7862@gmail.com>
2025-06-18 12:32:11 +0500

    Update db.php

--- app/db.php
+++ app/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "root", "travel_blog");
+$conn = new mysqli("db", "root", "root", "travel_blog");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
